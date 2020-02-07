dbf752e45e725f3ff434d4b514ed03a2511805da
Chris Rice <chrisrice202@gmail.com>
2020-02-07 11:53:09 -0600

    Seperate Mod Timed

--- Day 2 Project/Day 2 Project/Day 2 Project.cpp
+++ Day 2 Project/Day 2 Project/Day 2 Project.cpp
@@ -1,47 +1,96 @@
-/*
-Day 2 Project.cpp: Using Mod in multiple ways
-@author Chris Rice
-@date 1/31/2020
-*/
+#include <Windows.h>
+#include <ctime>
+
+CONST int NO_ITERATIONS = 100000;
+
+typedef long long int64; typedef unsigned long long uint64;
 
 #include <iostream>
+using namespace std;
 
-//Subtraction based mod function
 int Mod1(int a, int b) {
 
-    while (a - b >= 0) {
-        a = a - b;
-    }
+	while (a - b >= 0) {
+		a = a - b;
+	}
 
-    return a;
+	return a;
 
 }
 
-//Division based mod function
 int Mod2(int a, int b) {
 
-    return ( a - ((a / b) * b));
+	return (a - ((a / b) * b));
 
+}
+
+
+uint64 GetTimeMs64()
+{
+	FILETIME ft;
+	LARGE_INTEGER li;
+	GetSystemTimeAsFileTime(&ft);
+	li.LowPart = ft.dwLowDateTime;
+	li.HighPart = ft.dwHighDateTime;
+
+	uint64 ret = li.QuadPart;
+	ret -= 116444736000000000LL; /* Convert from file time to UNIX epoch time. */
+	ret /= 10000; /* From 100 nano seconds (10^-7) to 1 millisecond (10^-3) intervals */
+
+	return ret;
 }
 
 int main()
 {
 
-    int valueA = 400;
-    int valueB = 220;
+	double firstNo = 0.0;
+	double secondNo = 0.0;
+	double resultSubMethod = 0.0;
+	double resultDivMethod = 0.0;
+	uint64 startTime = 0.0;
+	uint64 endTime = 0.0;
+	uint64 nTickCountFunction1 = 0.0;
+	uint64 nTickCountFunction2 = 0.0;
 
-    //Unit test of both mod functions
-    std::cout << valueA << " % " << valueB << " equals " << Mod1(valueA, valueB) << "\n";
-    std::cout << valueA << " % " << valueB << " equals " << Mod2(valueA, valueB) << "\n";
+	std::srand(199); // use as seed for random generator
+
+	startTime = GetTimeMs64();
+
+	for (int i = 0; i < NO_ITERATIONS; i++)
+	{
+		// if (i % 10000 == 0) cout << i << endl;
+
+		// Get random numbers between 1 - 1001 (never want 0)
+		firstNo = std::rand() % 1000 + 1;
+		secondNo = std::rand() % 1000 + 1;
+
+		Mod1(firstNo, secondNo);
+
+
+	}
+	// Get the End time + add it to our counter
+	endTime = GetTimeMs64();
+	nTickCountFunction1 = endTime - startTime;
+
+
+	std::srand(199); // use as seed for random generator
+	startTime = GetTimeMs64();
+	for (int i = 0; i < NO_ITERATIONS; i++)
+	{
+		// if (i % 10000 == 0) cout << i << endl;
+
+		// Get random numbers between 1 - 1001 (never want 0)
+		firstNo = std::rand() % 1000 + 1;
+		secondNo = std::rand() % 1000 + 1;
+
+		Mod2(firstNo, secondNo);
+	}
+	endTime = GetTimeMs64();
+	nTickCountFunction2 = endTime - startTime;
+
+	std::cout << "\t\tMethod 1" << "\tMethod 2" << "\n";
+	std::cout << "Total Count:\t" << NO_ITERATIONS << "\t\t" << NO_ITERATIONS << "\n";
+	std::cout << "Total Time:\t" << nTickCountFunction1 << "ms\t\t" << nTickCountFunction2 << "ms\n";
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
